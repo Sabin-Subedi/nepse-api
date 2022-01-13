50d537be9afc701d01cb2a1b235b365b47a0436b
Sabin Subedi <69413078+Sabin-Subedi@users.noreply.github.com>
2022-01-13 10:02:06 +0545

    added socket io for realtime connectionss

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const https = require("https");
 const connectDB = require("./util/db");
 const NEPSE = require("./model/nepseModel");
 const cors = require("cors");
+const { Server } = require("socket.io");
 
 dotenv.config();
 
 connectDB();
 const app = express();
 
+const server = https.createServer(app);
+
 app.use(cors());
+
+const io = new Server(server);
 
 app.get("/api/nepse-live", async (req, res) => {
   try {
@@ -28,6 +34,27 @@ app.get("/api/nepse-live", async (req, res) => {
   }
 });
 
-app.listen(process.env.PORT || 3000, () => {
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  io.emit("getData", async (req, res) => {
+    try {
+      const liveData = await NEPSE.find();
+      if (!liveData) {
+        res.sendStatus(400).json({
+          message: "No data found",
+        });
+      }
+
+      res.status(200).json({
+        data: liveData,
+      });
+    } catch (err) {
+      res.json({ error: err });
+    }
+  });
+});
+
+server.listen(process.env.PORT || 3000, () => {
   console.log("Server is running on port 3000");
 });
